aed6637bd0faf68306895acb0c98fe12c46e8aad
ra2dd <radoslawpryw@gmail.com>
2024-01-10 12:17:37 +0100

    add post model, add model to admin

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    content = models.CharField(max_length=512)
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    creator = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=False,
+    )
+
+    class Meta:
+        ordering = ['-created']
+        get_latest_by = ['-created']
--- social/urls.py
+++ social/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
     path('', RedirectView.as_view(url='posts/', permanent=True)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
